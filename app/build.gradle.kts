aaadc1e879dbbccd7f956c5349d6a090253ff79c
Ganesh Suthar <ganeshsuthar123@gmail.com>
2024-01-13 13:56:04 +0530

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "app.anime.wallpapers"
         minSdk = 27
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/app/anime/wallpapers/ui/splash/SplashActivity.kt
+++ app/src/main/java/app/anime/wallpapers/ui/splash/SplashActivity.kt
@@ -3,31 +3,17 @@ package app.anime.wallpapers.ui.splash
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.work.BackoffPolicy
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkInfo.State.BLOCKED
-import androidx.work.WorkInfo.State.CANCELLED
-import androidx.work.WorkInfo.State.ENQUEUED
-import androidx.work.WorkInfo.State.FAILED
-import androidx.work.WorkInfo.State.RUNNING
-import androidx.work.WorkInfo.State.SUCCEEDED
-import androidx.work.WorkManager
 import app.anime.wallpapers.R
 import app.anime.wallpapers.ui.home.HomeActivity
 import app.anime.wallpapers.utils.InternetCheck
 import app.anime.wallpapers.utils.extensions.openActivity
 import app.anime.wallpapers.utils.extensions.showInfoDialog
-import app.anime.wallpapers.workers.FirebaseToken
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import com.google.android.play.core.appupdate.AppUpdateOptions
 import com.google.android.play.core.install.model.ActivityResult
 import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.UpdateAvailability
-import java.util.concurrent.TimeUnit
 
 
 class SplashActivity : AppCompatActivity() {
@@ -41,32 +27,34 @@ class SplashActivity : AppCompatActivity() {
         appUpdateManager = AppUpdateManagerFactory.create(this)
         initInAppUpdater()
     }
+
     private fun initInAppUpdater() {
         val appUpdateInfoTask = appUpdateManager.appUpdateInfo
 
-        val callback =  registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
-            if (result.resultCode == inAppUpdateCode) {
-                when (result.resultCode) {
-                    RESULT_CANCELED -> {
-                        showInfoDialog(
-                            R.drawable.ic_app_updating,
-                            R.string.updating_error
-                        ) {
-                            finish()
+        val callback =
+            registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
+                if (result.resultCode == inAppUpdateCode) {
+                    when (result.resultCode) {
+                        RESULT_CANCELED -> {
+                            showInfoDialog(
+                                R.drawable.ic_app_updating,
+                                R.string.updating_error
+                            ) {
+                                finish()
+                            }
                         }
-                    }
 
-                    ActivityResult.RESULT_IN_APP_UPDATE_FAILED -> {
-                        showInfoDialog(
-                            R.drawable.ic_app_updating,
-                            R.string.updating_error
-                        ) {
-                            finish()
+                        ActivityResult.RESULT_IN_APP_UPDATE_FAILED -> {
+                            showInfoDialog(
+                                R.drawable.ic_app_updating,
+                                R.string.updating_error
+                            ) {
+                                finish()
+                            }
                         }
                     }
                 }
             }
-        }
 
         appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
             when {
@@ -87,8 +75,9 @@ class SplashActivity : AppCompatActivity() {
                 else -> {
                     InternetCheck { internet ->
                         if (internet) {
-                            initWorkers()
-                        }else{
+                            openActivity<HomeActivity>()
+                            finish()
+                        } else {
                             showInfoDialog(R.drawable.ic_server_error, R.string.server_error) {
                                 finish()
                             }
@@ -101,49 +90,15 @@ class SplashActivity : AppCompatActivity() {
             it.printStackTrace()
             InternetCheck { internet ->
                 if (internet) {
-                    initWorkers()
-                }else{
+                    openActivity<HomeActivity>()
+                    finish()
+                } else {
                     showInfoDialog(R.drawable.ic_server_error, R.string.server_error) {
                         finish()
                     }
                 }
             }
         }
-    }
-
-    private fun initWorkers() {
-        val constraints =
-            Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
-        val tokenWorkRequest = OneTimeWorkRequestBuilder<FirebaseToken>()
-            .setConstraints(constraints)
-            .setBackoffCriteria(BackoffPolicy.EXPONENTIAL, 5, TimeUnit.SECONDS)
-            .build()
-        val workManager = WorkManager.getInstance(this)
-        workManager
-            .beginWith(tokenWorkRequest)
-            .enqueue()
-        workManager.getWorkInfoByIdLiveData(tokenWorkRequest.id).observe(this, Observer {
-            when (it.state) {
-                SUCCEEDED -> {
-                    openActivity<HomeActivity>()
-                    finish()
-                }
-
-                FAILED -> {
-                    showInfoDialog(R.drawable.ic_server_error, R.string.server_error) {
-                        finish()
-                    }
-                }
-
-                ENQUEUED -> {
-
-                }
-
-                RUNNING -> {}
-                BLOCKED -> {}
-                CANCELLED -> {}
-            }
-        })
     }
 
 }
